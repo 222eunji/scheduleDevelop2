4756e128109ab1271d445b01f08da3d9746315d0
Eunji Lee <aggee212@gmail.com>
2025-05-01 19:15:08 +0900

    project create

---
+++ src/main/java/com/example/scheduledevelop2/ScheduleDevelop2Application.java
@@ -0,0 +1,13 @@
+package com.example.scheduledevelop2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleDevelop2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleDevelop2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/scheduledevelop2/ScheduleDevelop2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduledevelop2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleDevelop2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
