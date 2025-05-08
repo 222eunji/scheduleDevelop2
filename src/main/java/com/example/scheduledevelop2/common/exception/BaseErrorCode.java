936e1a6f174a820d687aac15ff9954ecdcd7091a
Eunji Lee <aggee212@gmail.com>
2025-05-08 11:11:32 +0900

    feat: customException 구현

---
+++ src/main/java/com/example/scheduledevelop2/common/exception/BaseErrorCode.java
@@ -0,0 +1,7 @@
+package com.example.scheduledevelop2.common.exception;
+
+public interface BaseErrorCode {
+
+    ErrorResponse getReasonHttpStatus();
+
+}
---
+++ src/main/java/com/example/scheduledevelop2/common/exception/ErrorCode.java
@@ -0,0 +1,24 @@
+package com.example.scheduledevelop2.common.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    // Schedule
+    SCHEDULE_NOT_FOUND(404, "NON Found", "S001", "존재하지 않는 일정입니다."),
+
+    // Comment
+    COMMENT_NOT_FOUND(404, "NON Found", "C001", "존재하지 않는 댓글입니다.");
+
+    // Reply
+
+
+    private final int status;
+    private final String error;
+    private final String code;
+    private final String message;
+
+}
